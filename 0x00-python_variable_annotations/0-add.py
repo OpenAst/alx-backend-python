67930bdb172ac81d0679440cb44bb5031d23a63e
OpenAst <isrealme0@gmail.com>
2023-10-05 09:37:16 -0700

    add and concat functions

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Moduels task 0
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of a and b
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module concatenate two strings
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns two strings concatenated
+    """
+
+    return str1 + str2
