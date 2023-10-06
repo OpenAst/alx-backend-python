77d59711c94d57c1f85bc1bf4e4d58326c1fdcfa
OpenAst <isrealme0@gmail.com>
2023-10-06 13:04:17 -0700

    now really updated

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,18 +4,16 @@ This module update the following code
 to the correct codes
 """
 
-from typing import List, Tuple
+from typing import Tuple
 
-def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
-    zoomed_in: List[int] = [
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    zoomed_in: Tuple = tuple(
         item for item in lst
         for i in range(factor)
-    ]
-
+    )
     return zoomed_in
 
-array = [12, 72, 91]
+array = (12, 72, 91)  # Use tuple instead of list for input
 
 zoom_2x = zoom_array(array)
-
 zoom_3x = zoom_array(array, 3)
